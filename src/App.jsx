343cd5d529c2c570abc6791fcc9d11ed9dae5e55
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-11-22 19:30:53 +0600

    responsive fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ import Search from "./components/Search";
 
 export default function App() {
   return (
-    <div className="bg-neutral-900 min-h-screen px-4 py-4">
-      <Header />
-      <HeaderTitle />
-      <Search />
-      <div className="lg:flex lg:gap-3">
-        <div className="lg:flex">
-          <CurrentWeather />
+    <div className="bg-neutral-900 min-h-screen px-4 py-4 lg:flex lg:justify-center">
+      <div className="lg:flex lg:w-[80%] lg:justify-center lg:flex-col">
+        <Header />
+        <HeaderTitle />
+        <Search />
+        <div className="lg:flex lg:justify-center lg:gap-3">
+          <div className="lg:flex">
+            <CurrentWeather />
+          </div>
+          <HourlyForecast />
         </div>
-        <HourlyForecast />
       </div>
     </div>
   );
